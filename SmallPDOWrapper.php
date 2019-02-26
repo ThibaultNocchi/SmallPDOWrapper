4d393cd0c7d805aa670418b02481608b1b6d89d2
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-02-26 22:54:06 +0100

    Documentation.

--- SmallPDOWrapper.php
+++ SmallPDOWrapper.php
@@ -1,239 +1,360 @@
 <?php
 
-/* Updated on 22-02-2019 18:54 */
+/* Updated on 26-02-2019 22:51 */
 
-class DB{
+class SmallPDOWrapper{
 
-	private $_host;
-	private $_db;
-	private $_user;
-	private $_pass;
-	private $_handle;
+    private $_handle;
 
-	public function __construct($host, $db, $user, $pass){ // Enregistre les paramètres en variables locales et ouvre la connexion
-		$this->setHost($host);
-		$this->setDb($db);
-		$this->setUser($user);
-		$this->setPass($pass);
+    /**
+     * __construct
+     * 
+     * Connects to the DB and stores the handle.
+     *
+     * @see openDb
+     *
+     * @return void
+     */
+    public function __construct(string $host, string $db, string $user, string $pass){
+        $this->_handle = $this->openDb($host, $db, $user, $pass);
+    }
 
-		$this->_handle = $this->openDb($this->_host, $this->_db, $this->_user, $this->_pass);
+    /**
+     * __destruct
+     *
+     * Destroys the associated handle and PDO automatically closes the connection when the handle is destroyed.
+     * 
+     * @return void
+     */
+    public function __destruct(){
+        $this->_handle = null;
+    }
 
-	}
+    /**
+     * openDb
+     * 
+     * Opens the connection to the DB and returns the handle.
+     *
+     * @param  string $host Database's host.
+     * @param  string $db Database's name.
+     * @param  string $user Database's user.
+     * @param  string $pass Database's password.
+     *
+     * @return PDO Handle to the PDO connection.
+     */
+    private function openDb(string $host, string $db, string $user, string $pass) : PDO{
+        $handle = false;
+        $pdo_options[PDO::ATTR_EMULATE_PREPARES] = false;
+        $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+        $pdo_options[PDO::ATTR_DEFAULT_FETCH_MODE] = PDO::FETCH_ASSOC;
+        $handle = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8mb4', $user, $pass, $pdo_options);
+        return $handle;
+    }
 
-	public function __destruct(){ // Ferme la connexion en détruisant le handle
-		$this->_handle = null;
-	}
+    /**
+     * isTableInDatabase
+     *
+     * Returns whether the table is already in the database.
+     * 
+     * @param  string $name Name of the table.
+     *
+     * @return bool
+     */
+    public function isTableInDatabase(string $name) : bool{
+        $query = "SELECT * FROM information_schema.tables WHERE table_name = :name";
+        $results = $this->query($query, ["name"=>$name]);
+        if(count($results) > 0) return true;
+        else return false;
+    }
 
-	private function openDb($host, $db, $user, $pass){ // Ouvre la connexion et renvoie le handle pour l'utiliser plus tard
-		$handle = false;
-		try {
-			$pdo_options[PDO::ATTR_EMULATE_PREPARES] = false;
-    		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-    		$pdo_options[PDO::ATTR_DEFAULT_FETCH_MODE] = PDO::FETCH_ASSOC;
-    		$handle = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8mb4', $user, $pass, $pdo_options);
-		}
-		catch(Exception $e) {
-    		die('Erreur : '.$e->getMessage());
-		}
-		return $handle;
-	}
 
-	public function isTableInDatabase($name){
-		$query = "SELECT * FROM information_schema.tables WHERE table_name = :name";
-		$results = $this->query($query, ["name"=>$name]);
-		if(count($results) > 0) return true;
-		else return false;
-	}
+    /**
+     * createTable
+     * 
+     * Creates a table with these parameters.
+     * 
+     * @param  string $name Table's name.
+     * @param  array $cols Columns names as keys and their types and other parameters as values.
+     * @param  array $constraints List of different constraints to apply to the table (foreign keys for example).
+     * @param  string $encoding String encoding type (default: utf8).
+     *
+     * @return bool False if the creation was cancelled, true if it succeeded. PDO can also throw an exception if it fails.
+     */
+    public function createTable(string $name, array $cols, array $constraints = null, string $encoding = null) : bool{
 
-	public function createTable($name, $cols, $constraints = null, $encoding = null){
+        if($this->isTableInDatabase($name)) return false;
 
-		if($this->isTableInDatabase($name)) return false;
+        $lines = [];
+        foreach ($cols as $key => $value) {
+            array_push($lines, $key . " " . $value);
+        }
+        $lines = join(',',$lines);
+        $query = "CREATE TABLE " . $name . " (" . $lines;
 
-		$lines = [];
-		foreach ($cols as $key => $value) {
-			array_push($lines, $key . " " . $value);
-		}
-		$lines = join(',',$lines);
-		$query = "CREATE TABLE " . $name . " (" . $lines;
+        if($constraints != null && $constraints != []){
+            $lines = join(',',$constraints);
+            $query .= ",".$lines;
+        }
 
-		if($constraints != null && $constraints != []){
-			$lines = join(',',$constraints);
-			$query .= ",".$lines;
-		}
+        $query .= ")";
 
-		$query .= ")";
+        if($encoding == null || $encoding == "") $encoding = "utf8";
+        $query .= " CHARACTER SET ".$encoding;
 
-		if($encoding != null && $encoding != "")
-			$query .= " CHARACTER SET ".$encoding;
+        $this->queryNoFetch($query);
+        return true;
+    }
 
-		$this->queryNoFetch($query);
-		return true;
-	}
+    /**
+     * query
+     * 
+     * Sends the query to the database with the specified named parameters.
+     * Ex.:
+     * SELECT * FROM MyTable WHERE ID = :param_id
+     * $parameters = ["param_id" => 2]
+     * 
+     * Results are like this:
+     * $results = [0 => ["ID" => 2, "Name" => "Marc-Emmanuel"], 1 => ["ID" => 2, "Name" => "Jean-Lou"], ...]
+     * 
+     * @param  string $query Query to execute.
+     * @param  array $parameters Array of named parameters. Keys are the parameters names and the values, their values.
+     *
+     * @return array List of arrays representing lines resulting from the query.
+     */
+    public function query(string $query, array $parameters = []) : array{
+        $req = $this->_handle->prepare($query);
+        $err = $req->execute($parameters);
+        $ret = [];
+        while($data = $req->fetch()){
+            array_push($ret, $data);
+        }
+        $req->closeCursor();
+        return $ret;
+    }
 
-	public function query($query, $parameters = []){ // Effectue la requête $query avec les paramètres $parameters. Les clés doivent porter le nom des paramètres // Erreur : 1 = Mauvaise query
-		$req = $this->_handle->prepare($query);
-		if(!$req) return 1;
-		$err = $req->execute($parameters);
-		$ret = [];
-		while($data = $req->fetch()){
-			array_push($ret, $data);
-		}
-		$req->closeCursor();
-		return $ret; // Retourne un tableau avec tous les résultats
-	}
+    /**
+     * queryNoFetch
+     * 
+     * Sends the query to the database with the specified named parameters, without fetching results from it.
+     * Used to execute INSERT, UPDATES, etc. statements.
+     *
+     * @see query
+     *
+     * @return void
+     */
+    public function queryNoFetch(string $query, array $parameters = []){
+        $req = $this->_handle->prepare($query);
+        $err = $req->execute($parameters);
+    }
 
-	public function queryNoFetch($query, $parameters = []){ // Effectue la requête $query avec les paramètres $parameters. Les clés doivent porter le nom des paramètres. Cette fonction ne fetch pas, donc pas d'erreur avec update ou insert...s // Erreur : 1 = Mauvaise query
-		$req = $this->_handle->prepare($query);
-		if(!$req) return 1;
-		$err = $req->execute($parameters);
-		return 0;
-	}
+    /**
+     * retrieveCol
+     * 
+     * Retrieves all values from a column.
+     *
+     * @param  string $table Table to use.
+     * @param  string $col Column values to retrieve.
+     *
+     * @return array List of values in the specified column.
+     */
+    public function retrieveCol(string $table, string $col) : array{
+        $query = "SELECT ".$col." AS col FROM ".$table;
+        $req = $this->_handle->prepare($query);
+        $req->execute(array());
+        $ret = [];
+        while($data = $req->fetch()){
+            array_push($ret, $data["col"]);
+        }
+        return $ret;
+    }
 
-	public function retrieveCol($table, $col){ // Récupère les valeurs d'une colonne $col d'une table $table en tant que clés d'un tableau qui est retourné
-		$query = "SELECT ".$col." AS col FROM ".$table;
-		$req = $this->_handle->prepare($query);
-		$req->execute(array());
-		$ret = [];
-		while($data = $req->fetch()){
-			$ret[$data['col']] = 1;
-		}
-		return $ret;
-	}
+    /**
+     * insertDatas
+     * 
+     * Insert a line of datas into the specified table.
+     *
+     * @param  mixed $table Table used to insert the values into.
+     * @param  mixed $datas Array of named parameters. Keys are the columns names and the values, their values.
+     * 
+     * Ex.:
+     * $parameters = ["ID" => 1, "Name" => "Christian"]
+     *
+     * @return void
+     */
+    public function insertDatas(string $table, array $datas){
 
-	public function insertDatas($table, $datas){ // Requête préparée INSERT
-		$req = 'INSERT INTO '.$table.'('; // $table est le nom de la table
-		$d = false;
-		foreach ($datas as $key => $value) { // $datas sont les paramètres en tableau avec en clé le nom de la colonne et en valeur bah la valeur
-			if($d){
-				$req .= ', ';
-			}
-			$req .= $key;
-			$d = true;
-		}
-		$req .= ') VALUES(';
-		$d = false;
-		foreach ($datas as $key => $value) {
-			if($d){
-				$req .= ', ';
-			}
-			$req .= ':'.$key;
-			$d = true;
-		}
-		$req .= ')';
-		
-		$req = $this->_handle->prepare($req);
-		$req->execute($datas);
+        $req = 'INSERT INTO '.$table.'(';
 
-	}
+        $columns = [];
+        foreach ($datas as $key => $value) {
+            array_push($columns, $key);
+        }
+        $req .= join(",", $columns);
 
-	public function insertMultipleValues($table, $datas){ // Insertion de plusieurs données dans une table. Le tableau doit être 2D. $datas[0] = ["col1", "col2", "col3"] et $datas[ligne] = [valeur1, valeur2, valeur3]
+        $req .= ') VALUES(';
 
-		$req = "INSERT IGNORE INTO ".$table."("; // Utilisation du mot clé IGNORE pour éviter les erreurs si une clé unique existe déjà
-		$d = false; // Permet d'ajouter le délimiteur si besoin
+        $values = [];
+        foreach ($datas as $key => $value) {
+            array_push($values, ':' . $key);
+        }
+        $req .= join(",", $values);
 
-		foreach ($datas[0] as $key => $value) { // Parcourt les noms de colonnes pour les ajouter à la requête
-			if($d) $req .= ', '; // Si ce n'est pas la première valeur, on ajoute une virgule
-			$req .= $value; // On ajoute le nom de la colonne
-			$d = true; // On met à true pour qu'un délimiteur soit mis avant la prochaine valeur
-		}
+        $req .= ')';
+        
+        $req = $this->_handle->prepare($req);
+        $req->execute($datas);
 
-		$req .= ") VALUES"; // On ferme la liste de colonnes et on prépare l'arrivée des valeurs
+    }
 
-		array_shift($datas); // On retire le premier tableau avec les noms de colonnes
+    /**
+     * insertMultipleValues
+     * 
+     * Insert multiple lines of data at once.
+     * Datas is a 2D array, with the first dimension being different lines, and each line being an array of values to insert.
+     * Datas[0] also doesn't have values, it has the names of the different columns. And values afterward needs to be in the same position as the columns names in Datas[0].
+     *
+     * Ex.:
+     * $datas =
+     * [
+     *  0 => ["ID", "Name"],
+     *  1 => [0, "Daniel"],
+     *  2 => [1, "Sebastian"],
+     *  3 => [2, "Max"],
+     *  4 => [232, "Lewis"]
+     * ]
+     * 
+     * @param  string $table Table's name.
+     * @param  array $datas Datas to insert, as explicited above.
+     *
+     * @return void
+     */
+    public function insertMultipleValues(string $table, array $datas){
 
-		$datasToSend = []; // On prépare le tableau de données
+        $req = "INSERT IGNORE INTO ".$table."("; // Ignoring already used keys.
 
-		$d = false; // On prépare le booléen de délimiteur entre les différentes données de ligne
+        $columns = [];
+        foreach ($datas[0] as $key => $value) { // Specifies the columns to be inserted.
+            array_push($columns, $value);
+        }
+        $req .= join(",", $columns);
 
-		foreach($datas as $key => $value) { // On parcourt chaque ligne
-			$d2 = false; // On prépare le délimiteur entre chaque élément de la ligne
-			if($d) $req .= ", "; // On ajoute le délimiteur si ce n'est pas la première ligne
-			$req .= "("; // On ouvre l'insertion des valeurs de la ligne
+        $req .= ") VALUES";
 
-			foreach ($value as $key2 => $value2) { // Pour chaque valeur de la ligne
-				if($d2) $req .= ", "; // On ajoute le délimiteur si c'est pas la première valeur
-				$req .= "?"; // On met un point d'interrogation pour la requête préparée
-				array_push($datasToSend, $value2); // On ajoute la valeur à l'array 1D à envoyer
-				$d2 = true; // On met à true pour qu'un délimiteur soit mis avant la prochaine valeur
-			}
+        array_shift($datas); // We remove the list of columns names.
 
-			$req .= ")"; // Fin de la ligne
-			$d = true; // On met à true pour qu'un délimiteur soit mis avant la prochaine valeur
-		}
+        $datasToSend = []; // On prépare le tableau de données.
 
-		$req = $this->_handle->prepare($req);
-		$req->execute($datasToSend); // Envoi de la requête
+        $lines = [];
+        foreach($datas as $key => $value) { // For each line to add.
 
-	}
+            $values = [];
+            foreach ($value as $key2 => $value2) { // For each value of a line.
+                array_push($datasToSend, $value2); // We add the value to the list of parameters.
+                array_push($values, "?");
+            }
+            array_push($lines, "(" . join(",",$values) . ")");
+        }
+        $req .= join(",", $lines);
+        
+        $req = $this->_handle->prepare($req);
+        $req->execute($datasToSend);
 
-	public function updateDatasOnKey($table, $datas, $whereKey){ // Requête préparée UPDATE avec un WHERE sur une clé précise
-		$req = 'UPDATE '.$table.' SET '; // $table est le nom de la table
-		$d = false;
-		foreach ($datas as $key => $value) { // $datas est un tableau contenant tous les paramètres avec en clé le nom de la colonne et en valeur la valeur
-			if($key == $whereKey){ continue; } // $whereKey est une chaîne avec le nom de la clé qui fait le where (WHERE $whereKey = :$whereKey)
-			if($d){ $req .= ', '; }
-			$req .= $key." = :".$key;
-			$d = true;
-		}
-		$req .= " WHERE ".$whereKey." = :".$whereKey;
+    }
 
-		$req = $this->_handle->prepare($req);
-		$req->execute($datas);
-	}
+    /**
+     * updateDatasOnKey
+     * 
+     * Updates lines specified by the where key with the given datas.
+     * The value of the where key is to be put into the datas.
+     * 
+     * If you want to update where ID = 2,
+     * $whereKey = "ID"
+     * $datas = ["ID" => 2, "Name" => "Kimi"]
+     *
+     * @param  string $table Table's name.
+     * @param  array $datas Datas to put into updated lines.
+     * @param  string $whereKey Key to select lines.
+     *
+     * @return void
+     */
+    public function updateDatasOnKey(string $table, array $datas, string $whereKey){
+        $req = 'UPDATE '.$table.' SET ';
+        
+        $columns = [];
+        foreach ($datas as $key => $value) {
+            if($key == $whereKey){ continue; }
+            array_push($columns, $key . " = :" . $key);
+        }
+        $req .= join(",", $columns);
+        $req .= " WHERE ".$whereKey." = :".$whereKey;
+        
+        $req = $this->_handle->prepare($req);
+        $req->execute($datas);
+    }
 
-	public function truncateTable($table){ // Reset la table $table
-		$req = $this->_handle->query("TRUNCATE TABLE ".$table);
-	}
+    /**
+     * truncateTable
+     * 
+     * Truncates (empty) the given table.
+     *
+     * @param  string $table Table to truncate.
+     *
+     * @return void
+     */
+    public function truncateTable(string $table){
+        $this->_handle->query("TRUNCATE TABLE ".$table);
+    }
 
-	public function doesKeyExist($table, $datas){ // Vérifie si une ligne existe satisfaisant les données existe
+    /**
+     * doesKeyExist
+     * 
+     * Checks if there is at least one line corresponding to the given parameters.
+     *
+     * @param  string $table Table's name.
+     * @param  array $datas Datas to check against.
+     *
+     * @return bool Whether the parameters are in the table or not.
+     */
+    public function doesKeyExist(string $table, array $datas) : bool{
 
-		$req = 'SELECT * FROM '.$table.' WHERE ';
-		$d = false;
-		foreach ($datas as $key => $value) {
-			if($d){ $req .= ' AND '; }
-			$req .= $key." = :".$key;
-			$d = true;
-		}
-		
-		$req = $this->_handle->prepare($req);
-		$req->execute($datas);
+        $req = 'SELECT COUNT(*) AS total FROM '.$table.' WHERE ';
+        
+        $columns = [];
+        foreach ($datas as $key => $value) {
+            array_push($columns, $key . " = :" . $key);
+        }
+        $req .= join(" AND ", $columns);
+        
+        $req = $this->_handle->prepare($req);
+        $req->execute($datas);
 
-		$res = $req->fetch();
-		$res = boolval($res);
-		$req->closeCursor();
+        $res = $req->fetch();
+        $res = $res["total"] > 0;
+        $req->closeCursor();
 
-		return $res;
+        return $res;
 
-	}
+    }
 
-	public function deleteKey($table, $keys){
-		$query = "DELETE FROM ".$table." WHERE ";
-		$toInsert = [];
-		foreach ($keys as $key => $value) {
-			array_push($toInsert, $key." = :".$key);
-		}
-		$toInsert = join(" AND ", $toInsert);
-		$query .= $toInsert;
-		$this->queryNoFetch($query, $keys);
-	}
-
-	public function countRows($table, $datas = [], $whereKey = ""){ // Compte le nombre de lignes qui correpondent à la clé dans whereKey
-
-		$req = 'SELECT COUNT(*) AS nbr FROM '.$table;
-
-		if($whereKey != "" AND isset($datas[$whereKey])){
-			$req .= ' WHERE '.$whereKey.' = :'.$whereKey;
-		}
-
-		$datas = $this->query($req, $datas)[0]["nbr"];
-		return $datas;	
-
-	}
-
-	private function setHost($host){ $this->_host = $host; }
-	private function setDb($db){ $this->_db = $db; }
-	private function setUser($user){ $this->_user = $user; }
-	private function setPass($pass){ $this->_pass = $pass; }
+    /**
+     * deleteKey
+     * 
+     * Deletes lines corresponding to the given parameters.
+     *
+     * @param  string $table Table's name.
+     * @param  array $keys Datas to check against.
+     *
+     * @return void
+     */
+    public function deleteKey(string $table, array $keys){
+        $query = "DELETE FROM ".$table." WHERE ";
+        $columns = [];
+        foreach ($keys as $key => $value) {
+            array_push($columns, $key . " = :" . $key);
+        }
+        $query .= join(" AND ", $columns);
+        $this->queryNoFetch($query, $keys);
+    }
 
 }
 
